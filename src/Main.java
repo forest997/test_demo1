76cda8d294ef86f6f367366e2a58f289e8bbdeb7
forest997 <w654406986@qq.com>
2019-12-10 10:01:00 +0800

    edit main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        TestVO testVO = new TestVO();
+        System.out.println(testVO);
         System.out.println("Hello World!");
     }
 }
