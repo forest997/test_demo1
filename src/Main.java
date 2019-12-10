4a50f9459909d505e6fdfcb3e96f12f80d580a41
forest997 <w654406986@qq.com>
2019-12-10 10:08:49 +0800

    edit main again

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-//        TestVo testVo = new TestVo();
-//        System.out.println(testVo);
+        TestVO testVO = new TestVO();
+        System.out.println(testVO);
         System.out.println("Hello World!");
     }
 }
